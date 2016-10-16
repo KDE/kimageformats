09b1ac5a160a37e1ef567a73e5e2c65cdeb2bb48
Allen Winter <winter@kde.org>
2016-10-16 11:50:18 -0400

    imageformats/kra.h - overrides for KraPlugin capabilities() and create()

--- src/imageformats/kra.h
+++ src/imageformats/kra.h
@@ -29,11 +29,10 @@ class KraPlugin : public QImageIOPlugin
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QImageIOHandlerFactoryInterface" FILE "kra.json")
 
 public:
-    Capabilities capabilities(QIODevice *device, const QByteArray &format) const;
-    QImageIOHandler *create(QIODevice *device, const QByteArray &format = QByteArray()) const;
+    Capabilities capabilities(QIODevice *device, const QByteArray &format) const Q_DECL_OVERRIDE;
+    QImageIOHandler *create(QIODevice *device, const QByteArray &format = QByteArray()) const Q_DECL_OVERRIDE;
 };
 
 
 
 #endif
-
