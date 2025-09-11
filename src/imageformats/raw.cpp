95f0d15e14ecb7050a26d45daf3e8ad0884e60e9
Mirco Miranda <mircomir@gmail.com>
2025-09-11 11:52:21 +0200

    RAW: Disable broken stream protection
    It seems that on BSD the definition introduced with MR !384 crashes Telegram.

--- src/imageformats/raw.cpp
+++ src/imageformats/raw.cpp
@@ -27,7 +27,7 @@
  * By defining RAW_IGNORE_BROKEN_STREAMS removes this protection, leaving
  * LibRaw in complete control of error handling (which is mostly absent).
  */
-// #define RAW_DISABLE_BROKEN_STREAM_PROTECTION
+#define RAW_DISABLE_BROKEN_STREAM_PROTECTION
 #endif
 
 #ifdef QT_DEBUG
